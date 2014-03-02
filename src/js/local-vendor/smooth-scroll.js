3e707797ddf79be08a8b991fe16bdb054120f59e
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-03-02 04:43:54 +0000

    Implement smooth-scrolling for anchor tags

---
+++ src/js/local-vendor/smooth-scroll.js
@@ -0,0 +1,29 @@
+define(['domReady','jquery','jqueryEasing'], function(domReady) {
+
+  var smoothScroll = function(evt){
+    evt.preventDefault();
+    var anchor = $(evt.target).attr('href');
+
+    if (typeof anchor === 'undefined' || anchor === '') {
+      return;
+    }
+
+    try {
+      console.log('anchor',$(anchor).offset().top);
+      $('html,body').stop().animate({
+        scrollTop: (anchor === '#top' ? 0 : $(anchor).offset().top)
+      },750, 'easeInOutExpo');
+
+      setTimeout(function(){
+        window.location.hash = anchor;
+      }, 850);
+    } catch (error) {
+      window.location.hash = anchor;
+    }
+    return false;
+  };
+
+  domReady(function(){
+    $('a[href^="#"]').on('click', smoothScroll);
+  });
+});
--- src/js/main.js
+++ src/js/main.js
@@ -4,17 +4,20 @@
     paths: {
       domReady: 'vendor/domReady',
       jquery: 'vendor/jquery.min',
+      jqueryEasing: 'vendor/jquery.easing.min',
       drawPath: 'local-vendor/draw-path',
       css3: 'local-vendor/css3',
     },
     shim: {
       jquery: { exports: '$' },
+      jqueryEasing: { deps: ['jquery'] },
       'vendor/form5-alignment.min': { deps: ['jquery'] },
     }
   });
 
   require([
     'vendor/form5-alignment.min',
+    'local-vendor/smooth-scroll',
     'init',
     'hero',
     'nav',
